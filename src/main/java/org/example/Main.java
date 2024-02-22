78fb7d2643b5c50071646d567f6f43c5cddcee19
pidgay <kacper.goch.pasp@gmail.com>
2024-02-22 16:15:06 +0100

    Added method to UnitService

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,7 @@ import org.example.engine.Game;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
--- src/main/java/org/example/api/UnitService.java
+++ src/main/java/org/example/api/UnitService.java
@@ -1,10 +1,12 @@
 package org.example.api;
 
 import org.example.units.Unit;
-import java.lang.reflect.*;
+
+import java.lang.reflect.InvocationTargetException;
 
 public class UnitService {
-    public static Class<?> returnUnitInstance(String className){
-
+    public Unit getInstanceByName(String name) throws NoSuchMethodException, InstantiationException, IllegalAccessException, ClassNotFoundException, InvocationTargetException {
+        Class<Unit> unitClass = (Class<Unit>) Class.forName("org.example.units." + name);
+        return unitClass.getDeclaredConstructor().newInstance();
     }
 }
